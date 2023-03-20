6c7caf1b8feac1650bb1d6e84c594245c079cc1f
Praveenive <124970010+Praveenive@users.noreply.github.com>
2023-03-21 02:32:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("change").innerHTML="10";
+
+setTimeout(function(){
+    document.getElementById("change").innerHTML="9";
+},1000);
+
+setTimeout(function(){
+    document.getElementById("change").innerHTML="8";
+},2000);
+setTimeout(function(){
+    document.getElementById("change").innerHTML="7";
+},3000);
+setTimeout(function(){
+    document.getElementById("change").innerHTML="6";
+},4000);
+setTimeout(function(){
+    document.getElementById("change").innerHTML="5";
+},5000);
+setTimeout(function(){
+    document.getElementById("change").innerHTML="4";
+},6000);
+setTimeout(function(){
+    document.getElementById("change").innerHTML="3";
+},7000);
+setTimeout(function(){
+    document.getElementById("change").innerHTML="2";
+},8000);
+setTimeout(function(){
+    document.getElementById("change").innerHTML="1";
+},9000);
+setTimeout(function(){
+    document.getElementById("change").innerHTML="Happy New Year";
+},10000);
